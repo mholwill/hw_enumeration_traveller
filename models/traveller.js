bbbba452fb629e514dad6dccfa25eb2acd7dcefd
mholwill <m_holwill@hotmail.co.uk>
2020-02-19 17:07:41 +0000

    Completed MVP and extensions

--- models/traveller.js
+++ models/traveller.js
@@ -15,20 +15,28 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) =>
+  journey.distance >= minDistance);
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  let uniqueTransport = function(value, index, self) {
+    return self.indexOf(value) === index;
+  };
 
-};
+  transportArray = this.journeys.map((journey) => journey.transport);
+  return transportArray.filter(uniqueTransport);
+  };
 
 
 module.exports = Traveller;
